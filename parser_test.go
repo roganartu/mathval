3ea7ccb707cfc2ff30e2d7724e9fde5756d03b6f
Tony Lee <github@roganartu.com>
2015-06-22 04:03:36 +1000

    Added recursive Equals checker helpers for parser testing

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,7 @@
 package mathval
 
 import (
+	"fmt"
 	"math/big"
 	"strings"
 	"testing"
@@ -23,6 +24,37 @@ func Test(t *testing.T) { TestingT(t) }
 type ParserSuite struct{}
 
 var _ = Suite(&ParserSuite{})
+
+type expressionChecker struct {
+	info *CheckerInfo
+}
+type opChecker expressionChecker
+type factorChecker expressionChecker
+type powerChecker expressionChecker
+type termChecker expressionChecker
+type numberChecker expressionChecker
+
+var ExpressionEquals Checker = &expressionChecker{
+	&CheckerInfo{Name: "ExpressionEquals", Params: []string{"obtained", "expected"}},
+}
+
+var OperatorEquals Checker = &opChecker{
+	&CheckerInfo{Name: "Operator", Params: []string{"obtained", "expected"}},
+}
+
+func (p *ParserSuite) TestParse(c *C) {
+	parser = NewParser(strings.NewReader("10^2")) //*4+1"))
+	expected := &Expression{
+		factor: &Factor{power: &Power{
+			term:  &Term{number: &Number{str: "10", val: big.NewRat(10, 1)}},
+			op:    &ExponentOp{op: POW},
+			power: &Power{term: &Term{number: &Number{str: "2", val: big.NewRat(2, 1)}}},
+		}},
+	}
+	exp, err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(exp, ExpressionEquals, expected)
+}
 
 func (t *ParserSuite) TestNewParser(c *C) {
 	parser = NewParser(strings.NewReader("test"))
@@ -288,4 +320,227 @@ func (p *ParserSuite) TestParseExpression(c *C) {
 	parser = NewParser(strings.NewReader("10+"))
 	_, err = parser.parseExpression()
 	c.Assert(err, NotNil)
+}
+
+// Checker methods
+func (e *expressionChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Expression
+	var ok bool
+	if obtained, ok = params[0].(*Expression); !ok {
+		return false, "Not an Expression"
+	}
+	if expected, ok = params[1].(*Expression); !ok {
+		return false, "Not an Expression"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		((obtained.expression == nil || expected.expression == nil) && obtained.expression != expected.expression) ||
+		((obtained.factor == nil || expected.factor == nil) && obtained.factor != expected.factor) ||
+		((obtained.op == nil || expected.op == nil) && obtained.op != expected.op) {
+		return false, "Expressions not equal"
+	}
+
+	if obtained.expression != nil {
+		if result, err = e.Check([]interface{}{obtained.expression, expected.expression}, names); !result {
+			return
+		}
+	}
+
+	if obtained.factor != nil {
+		f := &factorChecker{}
+		if result, err = f.Check([]interface{}{obtained.factor, expected.factor}, names); !result {
+			return
+		}
+	}
+
+	if obtained.op != nil {
+		o := &opChecker{}
+		if result, err = o.Check([]interface{}{obtained.op, expected.op}, names); !result {
+			return
+		}
+	}
+
+	return true, ""
+}
+func (e *expressionChecker) Info() *CheckerInfo {
+	return e.info
+}
+
+func (f *factorChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Factor
+	var ok bool
+	if obtained, ok = params[0].(*Factor); !ok {
+		return false, "Not a Factor"
+	}
+	if expected, ok = params[1].(*Factor); !ok {
+		return false, "Not a Factor"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		((obtained.power == nil || expected.power == nil) && obtained.power != expected.power) ||
+		((obtained.factor == nil || expected.factor == nil) && obtained.factor != expected.factor) ||
+		((obtained.op == nil || expected.op == nil) && obtained.op != expected.op) {
+		return false, "Factors not equal"
+	}
+
+	if obtained.factor != nil {
+		if result, err = f.Check([]interface{}{obtained.factor, expected.factor}, names); !result {
+			return
+		}
+	}
+
+	if obtained.power != nil {
+		p := &powerChecker{}
+		if result, err = p.Check([]interface{}{obtained.power, expected.power}, names); !result {
+			return
+		}
+	}
+
+	if obtained.op != nil {
+		o := &opChecker{}
+		if result, err = o.Check([]interface{}{obtained.op, expected.op}, names); !result {
+			return
+		}
+	}
+
+	return true, ""
+}
+func (f *factorChecker) Info() *CheckerInfo {
+	return f.info
+}
+
+func (p *powerChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Power
+	var ok bool
+	if obtained, ok = params[0].(*Power); !ok {
+		return false, "Not a Power"
+	}
+	if expected, ok = params[1].(*Power); !ok {
+		return false, "Not a Power"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		((obtained.term == nil || expected.term == nil) && obtained.term != expected.term) ||
+		((obtained.power == nil || expected.power == nil) && obtained.power != expected.power) ||
+		((obtained.op == nil || expected.op == nil) && obtained.op != expected.op) {
+		return false, "Powers not equal"
+	}
+
+	if obtained.power != nil {
+		if result, err = p.Check([]interface{}{obtained.power, expected.power}, names); !result {
+			return
+		}
+	}
+
+	if obtained.term != nil {
+		t := &termChecker{}
+		if result, err = t.Check([]interface{}{obtained.term, expected.term}, names); !result {
+			return
+		}
+	}
+
+	if obtained.op != nil {
+		o := &opChecker{}
+		if result, err = o.Check([]interface{}{obtained.op, expected.op}, names); !result {
+			return
+		}
+	}
+
+	return true, ""
+}
+func (p *powerChecker) Info() *CheckerInfo {
+	return p.info
+}
+
+func (t *termChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Term
+	var ok bool
+	if obtained, ok = params[0].(*Term); !ok {
+		return false, "Not a Term"
+	}
+	if expected, ok = params[1].(*Term); !ok {
+		return false, "Not a Term"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		((obtained.exp == nil || expected.exp == nil) && obtained.exp != expected.exp) ||
+		((obtained.number == nil || expected.number == nil) && obtained.number != expected.number) {
+		return false, "Terms not equal"
+	}
+
+	if obtained.exp != nil {
+		e := &expressionChecker{}
+		if result, err = e.Check([]interface{}{obtained.exp, expected.exp}, names); !result {
+			return
+		}
+	}
+
+	if obtained.number != nil {
+		n := &numberChecker{}
+		if result, err = n.Check([]interface{}{obtained.number, expected.number}, names); !result {
+			return
+		}
+	}
+
+	return true, ""
+}
+func (t *termChecker) Info() *CheckerInfo {
+	return t.info
+}
+
+func (n *numberChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Number
+	var ok bool
+	if obtained, ok = params[0].(*Number); !ok {
+		return false, "Not a Number"
+	}
+	if expected, ok = params[1].(*Number); !ok {
+		return false, "Not a Number"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		((obtained.val == nil || expected.val == nil) && obtained.val != expected.val) {
+		return false, "Numbers not equal"
+	}
+
+	if obtained.str != expected.str || obtained.val.Cmp(expected.val) != 0 {
+		return false, "Numbers not equal"
+	}
+
+	return true, ""
+}
+func (n *numberChecker) Info() *CheckerInfo {
+	return n.info
+}
+
+func (o *opChecker) Check(params []interface{}, names []string) (result bool, err string) {
+	var obtained, expected *Operator
+	if a, ok := params[0].(*AddOp); ok {
+		obtained = (*Operator)(a)
+		expected = (*Operator)(params[1].(*AddOp))
+	} else if a, ok := params[0].(*MultiplyOp); ok {
+		obtained = (*Operator)(a)
+		expected = (*Operator)(params[1].(*MultiplyOp))
+	} else if a, ok := params[0].(*ExponentOp); ok {
+		obtained = (*Operator)(a)
+		expected = (*Operator)(params[1].(*ExponentOp))
+	} else {
+		return false, "Not an Operator"
+	}
+
+	// Nil checks
+	if ((obtained == nil || expected == nil) && obtained != expected) ||
+		(obtained.op != expected.op) {
+		return false, fmt.Sprintf("Expected: %d\nObtained: %d", expected.op, obtained.op)
+	}
+
+	return true, ""
+}
+func (o *opChecker) Info() *CheckerInfo {
+	return o.info
 }
